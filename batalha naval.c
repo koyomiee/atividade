b3188009f32c5d1043de562451124fadd667c919
koyomiee <165352058+koyomiee@users.noreply.github.com>
2025-06-02 15:21:00 -0300

    Update batalha naval.c
    adição dos requisitos da fase média

--- batalha naval.c
+++ batalha naval.c
@@ -1,19 +1,35 @@
 #include <stdio.h>
-// iniciante
+// medio
 
 int main() {
     int tabuleiro[10][10] = {0}; 
 
-    int navio_horizontal[3] = {3, 3, 3};
-    int navio_vertical[3] = {3, 3, 3};
+    int navio_1[3] = {3, 3, 3};
+    int navio_2[3] = {3, 3, 3};
+    int navio_3[3] = {3, 3, 3}; //navio diagonal
+    int navio_4[3] = {3, 3, 3}; //navio diagonal
 
+    //navio 1 (horizontal)
     for (int i = 0; i < 3; i++){//adicionarei mais 1 a i até que seja 3 uuuUuuUUU
-        tabuleiro[1][4 + i] = navio_horizontal[i]; //inicializando e posicionando navios horizontais na linha 3 + o que ela precisa
+        tabuleiro[0][2 + i] = navio_1[i]; //inicializando e posicionando navios horizontais na linha 3 + o que ela precisa
     }
+
+    //navio 2 (vertical)
     for (int i = 0; i < 3; i++) {
-        tabuleiro[6 + i][4] = navio_vertical[i]; //inicializando e botando navios verticais na coluna 7 + o que ela precisa
+        tabuleiro[6 + i][0] = navio_2[i]; //inicializando e botando navios verticais na coluna 7 + o que ela precisa
     }//(os 3 são 3 navios ou um navio de 3 casas?)
     
+    //matrizes horizontais são [x][y+i], onde x é a linha e y é a coluna
+    //matrizes verticais são [x+i][y]?
+
+    //navio 3 (diagonal)
+    for (int i = 0; i < 3; i++){
+        tabuleiro[7 - i][7 + i] = navio_3[i];
+    }
+    //navio 4 (diagonal)
+     for (int i = 0; i < 3; i++){
+        tabuleiro[2 + i][5 - i] = navio_4[i];
+    }
     printf("\n===Tabuleiro de Batalha Naval===\n");
     printf("   ");
     for (int i = 0; i < 10; i++) {
