661e1bd42169eb728fddf231d000be4660b0abea
koyomiee <165352058+koyomiee@users.noreply.github.com>
2025-06-02 11:10:57 -0300

    Update xadrez.c (avançado)
    acho que não teve bug pra corrigir, só dei uma tapeada nos requisitos

--- xadrez.c
+++ xadrez.c
@@ -1,50 +1,80 @@
 #include <stdio.h>
-//médio
+//avançado
+//declarações recursivas, entre parenteses, eu declaro uma função que recebe um inteiro, indicando que são o número de casas restantes para mover.
+int movimento_torre(int casas_restantes);//atribuindo a int de casas restantes, que será chamada depois em cada caso
+int movimento_bispo(int casas_restantes);//é como avisar "ei, aqui vai ter um inteiro com valor predeterminado, eu não sei qual, mas vai ter!"
+int movimento_rainha(int casas_restantes);
+int movimento_cavalo(int casas_restantes);
+
 int main() {
-    //esses ints serão usados para dizer quantas casas a peça tem que mover.
-    //são usadas para serem comparadas com os contadores, atingindo o int, o contador para.
+    //eu declaro e dou valores a esses inteiros, com o numero determinado de casas. essas casas serão chamadas posteriormente
+    //por "casas_restantes"
     int casas_torre = 5;
     int casas_bispo = 5;
     int casas_rainha = 8;
-    int casas_cavalo = 3; //adicionando o movimento do cavalo
-    
-    printf("\n===Movimento da Torre (5 casas para direita)===\n");
-    for(int i = 1; i <= casas_torre; i++) {//int com valor um, será incrementado até atingir o número de casas_torre.
-        printf("direita\n");//vai imprimir 5 vezes, enquanto o loop é usado, até chegar em 5. não sei pq ele não pula pra baixo
-    }
-    
-    printf("\n===Movimento do Bispo (5 casas diagonal cima-direita)===\n");
-    int contador_bispo = 1;//declaração do inicio do contador (é engraçado que vc tenha que iniciar toda vez, não sei pq)
-    while(contador_bispo <= casas_bispo) {//enquento o contador bispo for igual ou menor a casas, ele vai imprimir cima direita e adicionar 1 ao contador
-        printf("cima, direita\n");
-        contador_bispo++;//pq ++ e não só +?
-    }
-    
-    printf("\n===Movimento da Rainha (8 casas para esquerda)===\n");
-    int contador_rainha = 1;
-    do {
-        printf("esquerda\n");
-        contador_rainha++;//aqui, ele vai "doar" a impressão de esquerda, e adicionar 1 no int, enquanto o contador for menor que casas.
-    }   while(contador_rainha <= casas_rainha);//pior que nem tem espaço no tabuleiro pra mover 8 casas mané
-    //não sei bem como isso é uma simulação, tipo, eu só to imprimindo direções e adicionando mais 1 ao cont.
-    //eu piloto em simuladores, geralmente eles calculam as coisas e transmitem em FFB até onde eu sei, será que é assim tbm?
-    printf("\n===Movimento do Cavalo (3 casas formando um L)===\n");
-    int contador_cavalo = 1;
-    int contador_cavalo_x = 1; //contador para o movimento horizontal
-    int contador_cavalo_y = 1; //contador para o movimento vertical
-    for (int i = 0; i <casas_cavalo; i++){//precisando usar um for e um while, as coisas complicam e eu fiz uma gambiarra
-        while (contador_cavalo_x <=2)//aqui eu to fazendo o trabalho manual, só permito o cavalo mover 2 casas
-        {
-            printf("baixo\n");
-            contador_cavalo_x++;
-        }
-        while (contador_cavalo_y <=1)//enquanto aqui ele só move uma casa
-        {
-            printf("esquerda\n");
-            contador_cavalo_y++;//eu tenho certeza que esse sistema vai voltar pra me amaldiçoar, usando um limite
-            //hard e não uma variável
-        }
-    }
-    
+    int casas_cavalo = 3;
+
+    //aqui eu as chamo, atribuindo cada função a sua variavel com o numero de casas restantes
+    movimento_torre(casas_torre);
+    movimento_bispo(casas_bispo);
+    movimento_rainha(casas_rainha);
+    movimento_cavalo(casas_cavalo);
+
     return 0;
+}
+
+// Implementações das funções
+int movimento_torre(int casas_restantes) {
+    printf("\n===Movimento da Torre (5 casas a direita)===\n");
+    for(int i = 0; i < casas_restantes; i++) {//caso simples de for, incrementa i até atingir casas_restantes, imprimindo "direita"
+        printf("direita\n");
+    }
+    return casas_restantes; //retorna número de movimentos
+}
+
+int movimento_bispo(int casas_restantes) {
+    printf("\n===Movimento do Bispo (5 casas diagonal cima-direita)===\n");
+    int contador = 0;
+    int contador_y = 0;
+    int contador_x = 0;
+    do
+    {//a atividade pede que o controle horizontal e vertical sejam separados, então fiz dois contadores, que adiocionam a si, a um contador geral e imprimem a direção
+        printf ("cima\n");
+        contador_y++;//fiz isso usando "do"
+        contador++;
+        printf ("direita\n");
+        contador_x++;
+        contador++;
+    } while (contador_y < casas_restantes && contador_x < casas_restantes);
+    
+    return contador; //retorna número de movimentos
+}
+
+int movimento_rainha(int casas_restantes) {
+    printf("\n===Movimento da Rainha (8 casas a esquerda)===\n");
+    int contador = 0;
+    do {//comentario comentario comentario
+        printf("esquerda\n");
+        contador++;//loop simples pra imprimir "esquerda", enquanto o contador for menor que casas_restantes
+    } while(contador < casas_restantes);
+    return contador; //retorna número de movimentos
+}
+
+int movimento_cavalo(int casas_restantes) {
+    printf("\n===Movimento do Cavalo (3 casas formando um L)===\n");
+    int movimentos = 0;
+    switch (movimentos < casas_restantes)
+    {//SWITCH!!!!
+    case 1:
+        printf("cima\n");
+        movimentos++;//uma casa pra cima
+        printf("cima\n");
+        movimentos++; //outra casaa para cima
+        printf("direita\n");
+        movimentos++; //uma casa para direita
+        break;
+    default:
+        break;
+    }
+    return movimentos; //retorna número de movimentos
 }
